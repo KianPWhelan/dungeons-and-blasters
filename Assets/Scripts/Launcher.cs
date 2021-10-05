aeeb6ddf5ac8abdc0b015613b87d7d30e1b93097
KSamsoe <60244208+KSamsoe@users.noreply.github.com>
2021-10-05 10:11:31 -0400

    Added basic UI for connecting to PUN server.

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -17,6 +17,13 @@ namespace Com.OfTomorrowInc.DMShooter
         [SerializeField]
         private byte maxPlayersPerRoom = 4;
 
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        [SerializeField]
+        private GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField]
+        private GameObject progressLabel;
+
         #endregion
 
 
@@ -51,7 +58,8 @@ namespace Com.OfTomorrowInc.DMShooter
         /// </summary>
         void Start()
         {
-            Connect();
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
         }
 
 
@@ -68,6 +76,9 @@ namespace Com.OfTomorrowInc.DMShooter
         /// </summary>
         public void Connect()
         {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+
             // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
             if (PhotonNetwork.IsConnected)
             {
@@ -98,6 +109,8 @@ namespace Com.OfTomorrowInc.DMShooter
 
         public override void OnDisconnected(DisconnectCause cause)
         {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
             Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
         }
 
