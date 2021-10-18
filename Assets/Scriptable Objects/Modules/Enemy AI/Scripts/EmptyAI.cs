8e2a75c38cbb3248ff43fb291077e32f57457063
KSamsoe <60244208+KSamsoe@users.noreply.github.com>
2021-10-18 11:32:30 -0400

    Added empty AI object.

---
+++ Assets/Scriptable Objects/Modules/Enemy AI/Scripts/EmptyAI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Plug and play AI that follows and attacks a target if it is within line of sight
+/// </summary>
+[CreateAssetMenu(menuName = "Modules/AI/Empty AI")]
+public class EmptyAI : EnemyAI
+{
+    public override void Tick(GameObject self, GameObject target, NavMeshAgent agent, Movement movement)
+    {}
+}
