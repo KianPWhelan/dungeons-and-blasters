98d38b26e77f6314dab1e81319f7c87bf4519439
KSamsoe <60244208+KSamsoe@users.noreply.github.com>
2021-10-20 15:54:33 -0400

    Added spawning sound effects to enemies

--- Assets/Scripts/Entities/Enemies/EnemyGeneric.cs
+++ Assets/Scripts/Entities/Enemies/EnemyGeneric.cs
@@ -32,6 +32,8 @@ public class EnemyGeneric : MonoBehaviour
 
     public int slotSize;
 
+    public GameObject spawnSoundEffect;
+
     public List<float> ranges;
 
     // Current valid state transitions
@@ -52,6 +54,14 @@ public class EnemyGeneric : MonoBehaviour
         Debug.Log(agent.ToString());
         startingSpeed = agent.speed;
         TryGetComponent(out statusEffects);
+
+        if(spawnSoundEffect != null)
+        {
+            var m_Sound = Instantiate(spawnSoundEffect, transform.position, Quaternion.identity);
+            var m_Source = m_Sound.GetComponent<AudioSource>();
+            float life = m_Source.clip.length / m_Source.pitch;
+            Destroy(m_Sound, life);
+        }
     }
 
     // Update is called once per frame
