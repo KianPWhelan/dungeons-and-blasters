d0c0a8e7b9a652fa207f38a460a4f53b7d52f0cd
KianPWhelan <94023417+KianPWhelan@users.noreply.github.com>
2021-11-28 12:31:53 -0500

    Combined the float and rotation scripts
    I also figured out how to get the float script to move!

--- Assets/Scriptable Objects/Modules/Weapons/Scripts/Float_Rotation_Script.cs
+++ Assets/Scriptable Objects/Modules/Weapons/Scripts/Float_Rotation_Script.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//To get the obj to move, create an empty parent and give it the script. Then move around the child. (or the other way around)
-public class Float_Script : MonoBehaviour
+
+public class Float_Rotation_Script : MonoBehaviour
 {
     public float amp;
     public float freq;
     Vector3 initPos;
+    [SerializeField] private Vector3 _rotation;
 
     private void Start()
     {
@@ -16,5 +17,6 @@ public class Float_Script : MonoBehaviour
     private void Update()
     {
         transform.position = new Vector3(initPos.x, Mathf.Sin(Time.time * freq) * amp + initPos.y, 0);
+        transform.Rotate(_rotation * Time.deltaTime);
     }
 }
