b49a35b408131a0eda3dfabbe2a2e7a4b71727e7
KSamsoe <60244208+KSamsoe@users.noreply.github.com>
2021-12-01 11:43:43 -0500

    Added alternate attack functionality for weapons.

--- Assets/Scriptable Objects/Modules/Weapons/Attacks/Scripts/Attack.cs
+++ Assets/Scriptable Objects/Modules/Weapons/Attacks/Scripts/Attack.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
 [CreateAssetMenu(menuName = "Modules/Weapons/Attacks/Attack")]
 public class Attack : ScriptableObject
