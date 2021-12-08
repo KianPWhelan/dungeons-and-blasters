b53d885a0e94aba4155b5b0540f9ca7927bcd9a5
KSamsoe <60244208+KSamsoe@users.noreply.github.com>
2021-12-08 10:19:51 -0500

    Session is now closed when DM starts game.

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
 public class StartGame : MonoBehaviour
 {
     private GameManager gameManager;
+    private NetworkRunner runner;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
+        runner = NetworkRunner.GetRunnerForGameObject(gameObject);
         //StartGameSequence();
     }
 
     public void StartGameSequence()
     {
         gameManager.SendRoomDataToClients();
+        runner.SessionInfo.IsOpen = false;
     }
 }
